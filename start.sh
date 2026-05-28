e34778eebbdc141371a29ad7afe9563bc9afa109
creepebucket <creepebucket@qq.com>
2026-05-29 00:29:42 +0800

    添加方便的安装和启动

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+if [ ! -d ".venv" ]; then
+  python3 -m venv .venv
+fi
+
+. .venv/bin/activate
+
+python3 -m pip install -r requirements.txt
+python3 -m playwright install chromium
+
+python3 main.py
